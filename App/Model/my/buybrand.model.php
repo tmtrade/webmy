6a8393c1d7770260c6b8d18688d704d829135ecd
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-06-22 03:37:49 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/webmy@10452 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

---
+++ App/Model/my/buybrand.model.php
@@ -0,0 +1,56 @@
+<?
+/**
+* 
+* 商标购买
+* 
+* @package	Module
+* @author	haydn
+* @since	2016-01-18
+*/
+class BuyBrandModel extends AppModel
+{
+    /**
+    * 记录商标购买
+    * 
+    * @access  public
+    * @author  haydn
+    * @since   2016-03-11
+    * @param   array		$array  数据包
+    * @return  int        	$id
+    */
+    public function add($array)
+    {
+        $data['userId']     = $array['userId'];
+        $data['mobile']     = $array['mobile'];
+        $data['tid']     	= $array['tid'];
+        $data['trademark']  = $array['trademark'];
+        $data['class']    	= $array['class'];
+        $data['remarks']    = $array['remarks'];
+        $data['name']    	= !empty($array['name']) ? $array['name'] : '';
+        $data['aid']    	= $array['aid'];
+        $data['subject']    = $array['subject'];
+        $data['recorddate'] = TIME;
+        $data['recordip']   = getClientIp();
+        $id                 = $this->create($data);
+        return $id;
+    }
+
+    /**
+     * 获取用户添加的数量
+     * @author  haydn
+     * @since   2016-03-11
+     * @param   string||int	$account||$uid  手机或用户id
+     * @param   int    		$trademark   	商标号
+     * @param   int        	$class   		商标类型
+     * @param   string      $field   		检查字段
+     * @return  bool
+     */
+    public function getBuyCount($account,$trademark,$class,$field = 'userId')
+    {
+		$r['eq']	= array($field => $account,'trademark' => $trademark,'class' => $class);
+		$data		= $this->findAll($r);
+		$count		= $data['total'];
+		return $count;
+    }
+}
+?>

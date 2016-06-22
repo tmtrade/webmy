780d07f332b67d032b8ad3882d6d00057c85c57e
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-06-22 03:51:13 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/webmy@10455 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Util/app.function.php
+++ App/Util/app.function.php
@@ -785,4 +785,34 @@ function verifyArrayKey($array,$param)
 	}
 	return $msg;
 }
+
+/**
+ * 得到客户端ip地址
+ * @param int $type
+ * @param bool $adv
+ * @return mixed
+ */
+function get_client_ip($type = 0,$adv=true) {
+	$type       =  $type ? 1 : 0;
+	static $ip  =   NULL;
+	if ($ip !== NULL) return $ip[$type];
+	if($adv){
+		if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+			$pos    =   array_search('unknown',$arr);
+			if(false !== $pos) unset($arr[$pos]);
+			$ip     =   trim($arr[0]);
+		}elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+			$ip     =   $_SERVER['HTTP_CLIENT_IP'];
+		}elseif (isset($_SERVER['REMOTE_ADDR'])) {
+			$ip     =   $_SERVER['REMOTE_ADDR'];
+		}
+	}elseif (isset($_SERVER['REMOTE_ADDR'])) {
+		$ip     =   $_SERVER['REMOTE_ADDR'];
+	}
+	// IP地址合法验证
+	$long = sprintf("%u",ip2long($ip));
+	$ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+	return $ip[$type];
+}
 ?>
